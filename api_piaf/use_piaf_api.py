6aa0fdd56372076ba4a040241d4ef1a5a50859ce
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-07-30 16:21:18 +0200

    Feat: add security check piaf response :sparkles:

--- api_piaf/use_piaf_api.py
+++ api_piaf/use_piaf_api.py
@@ -1,13 +1,17 @@
 import requests
 
 def ask_question_to_piaf(question):
+    print('asking piaf', question)
     url = "https://piaf.datascience.etalab.studio/models/1/doc-qa"
     data = {"questions": [f"{question}"], "top_k_reader": 3, "top_k_retriever": 5}
-    response = requests.post(url, json=data).json()['results'][0]['answers'][0]
+    response = requests.post(url, json=data).json()
     # text = response['answer']
     # proba = response['probability']
     # context = response['context']
-    return response
+    if not response['results'][0]['answers']:
+        return None
+    else:
+        return response['results'][0]['answers'][0]
 
 if __name__ == '__main__':
     response = ask_question_to_piaf("comment faire une carte d'identité ?")
