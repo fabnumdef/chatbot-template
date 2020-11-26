355934f827ac38ca3e875a18b1a35b0ffdefea58
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-11-26 20:09:01 +0100

    Feat: implement tree.json for covid :sparkles:

--- actions/actions.py
+++ actions/actions.py
@@ -9,34 +9,13 @@
 
 from typing import Any, Text, Dict, List, Union
 
-from rasa_sdk.events import AllSlotsReset
-
-from rasa_sdk.forms import FormAction
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from actions.use_piaf_api import ask_question_to_piaf
-from actions.tree import return_themes, return_sub_themes, return_directories, return_sub_directories, return_cards
+from actions.tree import return_tree
 
-class ActionAskPiaf(Action):
+class ActionSendTree(Action):
     def name(self):
-        return "action_ask_piaf"
-
-    def run(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-    ) -> List[Dict]:
-        if((tracker.latest_message)['intent']['confidence'] < 0.8):
-            question = (tracker.latest_message)['text']
-        else:
-            question = tracker.get_slot('question')
-
-        return ask_question_to_piaf(self, dispatcher, tracker, domain, question)
-
-class ActionSendThemes(Action):
-    def name(self):
-        return "action_send_themes"
+        return "action_send_tree"
 
     def run(
             self,
@@ -44,72 +23,11 @@ class ActionSendThemes(Action):
             tracker: Tracker,
             domain: Dict[Text, Any],
         ) -> List[Dict]:
-        themes = return_themes()
-        dispatcher.utter_message(text = "Sélectionnez le sujet de votre demande :", buttons = themes)
-        return []
-
-class ActionSendSubThemes(Action):
-    def name(self):
-        return "action_send_sub_themes"
-
-    def run(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-        ) -> List[Dict]:
-        theme = tracker.get_slot('theme')
-        sub_themes = return_sub_themes(theme)
-        dispatcher.utter_message(text = "Votre question concerne :", buttons = sub_themes)
-        return []
-
-class ActionSendDirectories(Action):
-    def name(self):
-        return "action_send_directories"
-
-    def run(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-        ) -> List[Dict]:
-        theme = tracker.get_slot('theme')
-        sub_theme = tracker.get_slot('sub_theme')
-        directories = return_directories(theme, sub_theme)
-        dispatcher.utter_message(text = "Choisissez parmis les dossiers suivants :", buttons = directories)
-        return []
-
-class ActionSendSubDirectories(Action):
-    def name(self):
-        return "action_send_sub_directories"
-
-    def run(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-        ) -> List[Dict]:
-        theme = tracker.get_slot('theme')
-        sub_theme = tracker.get_slot('sub_theme')
-        directory = tracker.get_slot('directory')
-        sub_directories = return_sub_directories(theme, sub_theme, directory)
-        dispatcher.utter_message(text = "Choisissez parmis les sous-dossiers suivants :", buttons = sub_directories)
-        return []
-
-class ActionSendCards(Action):
-    def name(self):
-        return "action_send_cards"
-
-    def run(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any],
-        ) -> List[Dict]:
-        theme = tracker.get_slot('theme')
-        sub_theme = tracker.get_slot('sub_theme')
-        directory = tracker.get_slot('directory')
-        sub_directory = tracker.get_slot('sub_directory')
-        cards = return_cards(theme, sub_theme, directory, sub_directory)
-        dispatcher.utter_message(text = "Choisissez parmis les fiches suivantes :", buttons = cards)
+        messages = return_tree(tracker.get_slot('tree_id'))
+        for message in messages:
+            print (message)
+            if 'buttons' in message:
+                dispatcher.utter_message(text = message['text'], buttons = message['buttons'])
+            else:
+                dispatcher.utter_message(text = message['text'])
         return []
--- actions/tree.py
+++ actions/tree.py
@@ -1,72 +1,40 @@
 import json
 
 with open('data/tree.json') as json_file:
-    data = json.load(json_file)['data']
+    data = json.load(json_file)
 
-def return_themes():
-    print('return themes')
-    buttons = []
-    for theme in data:
-        if theme['id'] is None:
-            id = 'null'
+def return_tree(item_id):
+    print(item_id)
+    messages = []
+    if not item_id:
+        item_id = "03240eb5-bc48-4c13-90e0-dcf79b442f08"
+    item = next((x for x in data if x['id'] == item_id), None)
+    while True:
+        print(item)
+        if 'choices' in item:
+            messages[-1]['buttons'] = generate_message(item)['buttons']
         else:
-            id = theme['id']
-        buttons.append({'payload': '/choose_theme{"theme":"' + id + '"}', 'title': theme['name']})
-    return buttons
+            messages.append(generate_message(item))
+        if 'next' in item and item['next']:
+            item = next((x for x in data if x['id'] == item['next']), None)
+        else:
+            break
 
-def return_sub_themes(theme_id: str):
-    print('return sub themes from theme : ', theme_id)
-    theme = next((x for x in data if x['id'] == theme_id), None)
-    buttons = []
-    for sub_theme in theme['data']:
-        if sub_theme['name'] is None:
-            id = 'null'
-        else:
-            id = sub_theme['name']
-        buttons.append(
-            {'payload': '/choose_sub_theme{"sub_theme":"' + id + '"}', 'title': sub_theme['name']})
-    return buttons
+    return messages
 
-def return_directories(theme_id: str, sub_theme_id: str):
-    print('return directories from theme : ', theme_id, sub_theme_id)
-    theme = next((x for x in data if x['id'] == theme_id), None)
-    sub_theme = next((x for x in theme['data'] if x['name'] == sub_theme_id), None)
-    buttons = []
-    for directory in sub_theme['data']:
-        if directory['id'] is None:
-            id = 'null'
-        else:
-            id = directory['id']
-        buttons.append(
-            {'payload': '/choose_directory{"directory":"' + id + '"}', 'title': directory['name']})
-    return buttons
 
-def return_sub_directories(theme_id: str, sub_theme_id: str, directory_id: str):
-    print('return sub directories from theme : ', theme_id, sub_theme_id, directory_id)
-    theme = next((x for x in data if x['id'] == theme_id), None)
-    sub_theme = next((x for x in theme['data'] if x['name'] == sub_theme_id), None)
-    directory = next((x for x in sub_theme['data'] if x['id'] == directory_id), None)
-    buttons = []
-    for sub_directory in directory['data']:
-        if sub_directory['name'] is None:
-            id = 'null'
-        else:
-            id = sub_directory['name']
-        buttons.append(
-            {'payload': '/choose_sub_directory{"sub_directory":"' + id + '"}', 'title': sub_directory['name']})
-    return buttons
+def generate_message(item):
+    message = {}
+    if 'choices' in item and item['choices']:
+        message['text'] = ''
+        message['buttons'] = generate_buttons(item)
+    elif item['type'] == 'Text':
+        message['text'] = item['name']
+    return message
 
-def return_cards(theme_id: str, sub_theme_id: str, directory_id: str, sub_directory_id: str):
-    print('return cards from theme : ', theme_id, sub_theme_id, directory_id, sub_directory_id)
-    theme = next((x for x in data if x['id'] == theme_id), None)
-    sub_theme = next((x for x in theme['data'] if x['name'] == sub_theme_id), None)
-    directory = next((x for x in sub_theme['data'] if x['id'] == directory_id), None)
-    sub_directory = next((x for x in directory['data'] if x['name'] == sub_directory_id), None)
+def generate_buttons(item):
     buttons = []
-    for card in sub_directory['data']:
-        if card['id'] is None:
-            id = 'null'
-        else:
-            id = card['id']
-        buttons.append({'payload': 'https://www.service-public.fr/particuliers/vosdroits/' + id, 'title': card['name']})
+    for choice in item['choices']:
+        sub_item = next((x for x in data if x['id'] == choice), None)
+        buttons.append({'payload': '/send_tree{"tree_id":"' + sub_item['next'] + '"}', 'title': sub_item['name']})
     return buttons
